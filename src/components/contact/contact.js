e54f959338ef2089064ed96b0f520245fb0df6f7
Stephen Chiang <stephen.chiang@webstep.no>
2020-08-18 19:44:53 +0200

    New flex rules for contact

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -6,18 +6,25 @@ import Email from "../../assets/email.svg"
 const contact = props => {
   const tempContactCopyPhone = `+47 968 49 108`
   const tempContactEmail = `antonia.nichole.ux@gmail.com`
+  const tempLinkedIn = `https://www.linkedin.com/in/nichole-antonia/`
+  const templLinkedInText = `linkedin.com/in/nichole-antonia/`
   return (
     <section className={css.Contact}>
-      <Email />
-      <div className={css.Contact__Info}>
-        <p>
-          <a href={`tel:${tempContactCopyPhone}`}>{tempContactCopyPhone}</a>
-        </p>
-        <p>
+      <ul className={css.Contact__Info}>
+        <li className={css.Info__Item}>
+          <Email />
           <a href={`mailto:${tempContactEmail}`}>{tempContactEmail}</a>
-        </p>
-      </div>
-      <LinkedIn />
+        </li>
+
+        <li className={css.Info__Item}>
+          <a href={`tel:${tempContactCopyPhone}`}>{tempContactCopyPhone}</a>
+        </li>
+
+        <li className={css.Info__Item}>
+          <LinkedIn />
+          <a href={tempLinkedIn}>{templLinkedInText}</a>
+        </li>
+      </ul>
     </section>
   )
 }
